d210e4bd500acd1038efee8b39e536503808ee4c
Avano Berlingieri <avanoberlingieri@gmail.com>
2026-02-02 16:24:46 -0500

    Initial Commit

---
+++ todo/todoApp.swift
@@ -0,0 +1,17 @@
+//
+//  todoApp.swift
+//  todo
+//
+//  Created by Avano Berlingieri on 2026-02-02.
+//
+
+import SwiftUI
+
+@main
+struct todoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
